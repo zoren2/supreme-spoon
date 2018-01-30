afcfef21eb34d3b7116707bd6def43812726e971
headzoo <sean@headzoo.io>
2018-01-30 06:10:25 -0500

    Adds composer.json

--- index.php
+++ index.php
@@ -7,22 +7,11 @@ use Zoren\SupremeSpoon\Config;
 
 date_default_timezone_set('UTC');
 
-/* 
- * The autoload function is called when you try to initialize a class which
- * does not exist. It's the responsbility of the autoloader to find the file
- * where the class is defined, and include it.
- */
-spl_autoload_register(function($class_name) {
-    // Converts class names eg:'Zoren\SupremeSpoon\RegExp' to a file path like 'Zoren/SupremeSpoon/RegExp'.
-    $file_name = str_replace("\\", "/", $class_name);
-    
-    // Include the class file from the 'src/' directory.
-    include(__DIR__ . "/src/${file_name}.php");
-});
+require(__DIR__ . '/vendor/autoload.php');
 
 // Instantiate template and helper classes
-$api = new RedditAPI();
-$config = new Config();
+$api      = new RedditAPI();
+$config   = new Config();
 $dbhelper = new DBHelper($config);
 $template = new TemplateRenderer(new RegExp());
 
